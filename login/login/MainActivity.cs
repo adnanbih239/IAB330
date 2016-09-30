70c717058426e8c25fa92595ca0a65c57fb9845b
kelvinyoung1011 <kelvinyoung1011@gmail.com>
2016-09-30 15:14:38 +1000

    login, profile, search
    basic UI for the profile, login, search page

---
+++ login/login/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace login
+{
+    [Activity(Label = "login", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button1 = FindViewById<Button>(Resource.Id.loginbutton);
+            Button button2 = FindViewById<Button>(Resource.Id.clearbutton);
+
+
+
+            button1.Click += delegate { button1.Text = string.Format("login", count++); };
+            button2.Click += delegate { button2.Text = string.Format("clear"); };
+        }
+    }
+}
+
---
+++ profile page/profile page/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace profile_page
+{
+    [Activity(Label = "profile_page", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+           // Button imageButton1 = FindViewById<Button>(Resource.Id.imageButton1);
+            Button update = FindViewById<Button>(Resource.Id.update);
+            Button save = FindViewById<Button>(Resource.Id.save);
+
+
+
+          //  imageButton1.Click += delegate { imageButton1.Text = string.Format("update image please"); };
+            update.Click += delegate { update.Text = string.Format("update profile please"); };
+            save.Click += delegate { save.Text = string.Format("save profile please"); };
+        }
+    }
+}
+
